8346e4fa19ddb6e7e0dcea7f2a309fbbc5ce1787
Muhammad Saber <106914858+MuhammadSaber25@users.noreply.github.com>
2025-07-25 19:24:09 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //(1)
+        Console.WriteLine("EraaSoft520");
+
+        //(2)
+        Console.WriteLine("Write your Fav number from 1 to 100");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Wow you number {num} is my fav also");
+
+    }
+}
