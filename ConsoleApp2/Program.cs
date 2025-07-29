2a1686d203c83117ee3b0055a672210f502f122d
Muhammad Saber <106914858+MuhammadSaber25@users.noreply.github.com>
2025-07-29 22:17:04 +0300

    New Task

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //****************************** (1) ********************************************//
+        //Console.WriteLine("Is the student absent ? (Yes / No)");
+        // string Absent = Console.ReadLine();
+        //Console.WriteLine("enter the student Score");
+        //int Score = int.Parse(Console.ReadLine());
+
+        //if (Score < 50 || Absent == "Yes")
+        //    Console.WriteLine("Fail");
+        //else if (Score >= 50)
+        //    Console.WriteLine("Sucessed");
+
+        //Console.WriteLine(Score < 50 || Absent == "Yes"); //absent OR Score < 50
+
+        //****************************** (2) ********************************************//
+        //Console.Write("Enter a number :");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //if (number < 0)
+        //{
+        //    Console.WriteLine("Number is -ve");
+        //}
+        //else if (number > 0)
+        //{
+        //    Console.WriteLine("Number is +ve");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Number is Zero");
+        //}
+        //****************************** (3) ********************************************//
+        //Console.Write("Enter a number :");
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //if (number %2== 0)
+        //{
+        //    Console.WriteLine("Number is even");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Number is odd");
+        //}
+    }
+}
---
+++ MultiplicationTask/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MultiplicationTask;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Grade Checker
+        //Console.Write("enter a mark (0–100) :");
+        //int grade = Convert.ToInt32(Console.ReadLine());
+
+        //if (grade >= 90 && grade <= 100)
+        //{
+        //    Console.WriteLine("Your grade is A");
+        //}
+        //else if (grade >= 80 && grade <= 89)
+        //{
+        //    Console.WriteLine("Your grade is B");
+        //}
+        //else if (grade >= 70 && grade <= 79)
+        //{
+        //    Console.WriteLine("Your grade is C");
+        //}
+        //else if (grade >= 60 && grade <= 69)
+        //{
+        //    Console.WriteLine("Your grade is D");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Your grade is F");
+        //}
+
+        //Multiplication Table
+        Console.Write("Enter a number to print its multiplication table: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+        while (i <= 12)
+        {
+            Console.WriteLine($"{x}x{i} = {i * x}");
+            i++;
+        }
+    }
+}
